2d683c1a025e91ed2714e479568a806b787a3f59
arekiu <alex.isaguilera@hotmail.com>
2025-04-29 14:38:25 +0200

    fix wall_x calculation with player pos

--- src/ray_casting/ray_drawer.c
+++ src/ray_casting/ray_drawer.c
@@ -27,33 +27,32 @@ t_texture *get_wall_texture(t_game *game)
 		return game->so_text;
 	else if (game->ray->side == EAST)
 		return game->ea_text;
-	else // WEST
+	else
 		return game->we_text;
 }
 
 void set_texture(t_game *game, t_texture *texture)
 {
-	t_ray *ray = game->ray;
+	t_ray	*ray;
+	double px;
+	double py;
 
+	ray = game->ray;
+	px = game->player->x / BLOCK;
+	py = game->player->y / BLOCK;
 	if (ray->side == EAST || ray->side == WEST)
-	{
-		ray->wall_x = game->player->y + ray->wall_dist * ray->dir_y;
-	}
+		ray->wall_x = py + ray->wall_dist * ray->dir_y;
 	else
-	{
-		ray->wall_x = game->player->x + ray->wall_dist * ray->dir_x;
-	}
+		ray->wall_x = px + ray->wall_dist * ray->dir_x;
 	ray->wall_x -= floor(ray->wall_x);
-
 	ray->text_x = (int)(ray->wall_x * (double)texture->width);
-
 	if ((ray->side == EAST && ray->dir_x > 0) || (ray->side == WEST && ray->dir_x < 0))
 		ray->text_x = texture->width - ray->text_x - 1;
 	else if ((ray->side == NORTH && ray->dir_y < 0) || (ray->side == SOUTH && ray->dir_y > 0))
 		ray->text_x = texture->width - ray->text_x - 1;
 }
 
-int	paint_line(t_game *game, int y_end, int y_start, int color) // or paint_texture_line
+int	paint_line(t_game *game, int y_end, int y_start, int color)
 {
 	while (y_start <= y_end) //the = fix problem showing pixel line at the bottom
 	{
@@ -69,40 +68,4 @@ int get_pixel(t_texture *texture, int x, int y)
 		return 0;
 	char *pixel = texture->addr + (y * texture->line_length + x * (texture->bpp / 8));
 	return *(int *)pixel;
-}
-
-int draw_texture(t_game *game)
-{
-	t_ray		*ray = game->ray;
-	t_texture	*texture = get_wall_texture(game);
-	double		step;
-	double		tex_pos;
-	int			color;
-	int			y;
-
-	set_texture(game, texture);
-	step = 1.0 * texture->height / ray->line_height;
-	tex_pos = (ray->draw_start - WIN_HEIGHT / 2 + ray->line_height / 2) * step;
-
-	y = ray->draw_start;
-	if (y < 0)
-	{
-		tex_pos += step * -y;
-		y = 0;
-	}
-	while (y < ray->draw_end && y < WIN_HEIGHT)
-	{
-		int tex_y = (int)tex_pos;
-		if (tex_y < 0)
-			tex_y = 0;
-		else if (tex_y >= texture->height)
-			tex_y = texture->height - 1;
-
-		color = get_pixel(texture, ray->text_x, tex_y);
-		put_pixel(ray->current_x, y, color, game);
-
-		tex_pos += step;
-		y++;
-	}
-	return y;
 }
